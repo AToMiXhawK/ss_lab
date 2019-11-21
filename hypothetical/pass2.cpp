95ab83a93776d30c4873a2c64b00802e80eeec9d
AToMiXhawK <githinmanesh@gmail.com>
2019-11-21 06:44:29 +0530

    Hypothetical Assembler: Pass2 under construction

--- hypothetical/pass2.cpp
+++ hypothetical/pass2.cpp
@@ -12,15 +12,19 @@ void pass2(int SIZE)
     vector<string> row; 
     string line;
     int STADR=0, LOCCTR=0;
-
+    stringstream INSTR;
+    int inst_len = 0;
+    INSTR<<"T^";
     while (getline(fin, line))
     {
         //cout<<endl<<line<<"\t";
+        int len = 0;
         row = tokenize(line);
         string instr ="";
         if(row[2]=="START")
         {
             STADR = str2int(row[3]);
+            INSTR<<setw(6)<<hex<<STADR<<"^";
             fout<<"H^"<<setw(6)<<row[1]<<"^"<<setw(6)<<setfill('0')<<hex<<STADR<<"^"<<setw(6)<<setfill('0')<<hex<<SIZE<<endl;
             cout<<"H^"<<setw(6)<<row[1]<<"^"<<setw(6)<<setfill('0')<<hex<<STADR<<"^"<<setw(6)<<setfill('0')<<hex<<SIZE<<endl;
             continue;
@@ -36,8 +40,7 @@ void pass2(int SIZE)
         else if(row[3]=="EQU")
             continue;
 
-        stringstream INSTR;
-        int OPCODE, XBPE = 0, DISPL = 0;
+        int OPCODE, DISPL = 0;
         int fmt=0;
 
         if(OPTAB[row[2]].exists=='y')
@@ -47,34 +50,51 @@ void pass2(int SIZE)
 
             if(fmt==1)
             {
-                cout<<setw(1)<<setfill('0')<<hex<<OPCODE<<"0"<<endl;
+                INSTR<<setw(1)<<setfill('0')<<hex<<OPCODE<<"0"<<endl;
+                len = 2;
             }
 
             else if(fmt == 2)
             {
-                cout<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[row[3]].num<<endl;
+                INSTR<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[row[3]].num<<endl;
+                len = 2;
             }
 
             else if(fmt == 3)
             {
                 vector<string> regops = stcut(row[3], ',');
-                cout<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[regops[0]].num<<REGTAB[regops[1]].num<<REGTAB[regops[2]].num<<endl;
+                INSTR<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[regops[0]].num<<REGTAB[regops[1]].num<<REGTAB[regops[2]].num<<endl;
+                len = 4;
             }
 
             else if(fmt == 4)
             {
                 vector<string> ops = stcut(row[3], ',');
-                int len; OPTAB[row[2]].format>2?len=2:len=1;
-                LOCCTR = str2int(row[0]) + len;
-                DISPL = SYMTAB[ops[1]].address - LOCCTR;
-                cout<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[ops[0]].num;
-                printf("%02hhx\n",DISPL);
+                if(SYMTAB[ops[1]].is_equ)
+                    DISPL = SYMTAB[ops[1]].address;
+                else
+                {
+                    int f_len; OPTAB[row[2]].format>2?f_len=2:f_len=1;
+                    LOCCTR = str2int(row[0]) + f_len;
+                    DISPL = SYMTAB[ops[1]].address - LOCCTR;
+                    if(DISPL<0)
+                        DISPL -= 0xffffff00;
+                }
+                INSTR<<setw(1)<<setfill('0')<<hex<<OPCODE<<REGTAB[ops[0]].num<<setw(1)<<setfill('0')<<hex<<DISPL<<endl;
+                len = 4;
+                //printf("%02hhx\n",DISPL);
             }
 
             else if(fmt == 5)
             {
-                cout<<setw(1)<<setfill('0')<<hex<<OPCODE<<setw(3)<<setfill('0')<<hex<<SYMTAB[row[3]].address<<endl;
+                INSTR<<setw(1)<<setfill('0')<<hex<<OPCODE<<setw(3)<<setfill('0')<<hex<<SYMTAB[row[3]].address<<endl;
+                len = 4;
             }
+        }
+
+        if(inst_len+len>60)
+        {
+            
         }
 
     }
